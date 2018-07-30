bc61b4c3a4c099c853964e88d9ae6c111dfcac91
Getulio Miranda <getulio.miranda2@gmail.com>
2018-07-30 12:20:45 +0200

    initial commit.

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,36 @@
+package com.gildedrose;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+import static org.testng.Assert.assertEquals;
+
+public class GildedRoseTest {
+
+    @Test
+    public void testUpdateQuality() {
+
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Conjured Mana Cake", 3, 6)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        StringJoiner sj = new StringJoiner("\n");
+        Arrays.stream(app.getItems()).forEach(item -> sj.add(item.toString()));
+        String expected = "+5 Dexterity Vest, 9, 19\n" +
+                "Aged Brie, 1, 1\n" +
+                "Elixir of the Mongoose, 4, 6\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
+                "Conjured Mana Cake, 1, 5";
+        assertEquals(sj.toString(), expected);
+    }
+}
